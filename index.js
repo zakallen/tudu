9c2279a1ee0f73258c0d70803ceb3852777814f9
zakallen <zak.allen1009@gmail.com>
2019-04-13 13:30:56 -0700

    added tudus

--- index.js
+++ index.js
@@ -3,4 +3,8 @@
 const chalk = require('chalk');
 const log = console.log;
 
+// tudu: recursively go through all files in a tree
+// tudu: output tudus as they are hit within files
+// tudu: print them out using chalk in a nice way
+
 log(chalk.blue('Hello') + ' World' + chalk.green('!'));
