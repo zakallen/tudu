f34081764c10e1aef252bad9bd7fbd9a43399ac9
zakallen <zak.allen1009@gmail.com>
2019-04-13 21:01:17 -0700

    using grep

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 #!/usr/bin/env node
 
 const chalk = require('chalk');
+const { exec } = require('child_process');
 const log = console.log;
 
-// tudu: recursively go through all files in a tree
-// tudu: output tudus as they are hit within files
 // tudu: print them out using chalk in a nice way
+
+exec("grep -rnIF 'tudu:' .", (error, stdout, stderr) => {
+  if (error) {
+    console.error(`exec error: ${error}`);
+    return;
+  }
+  log(`stdout: ${stdout}`);
+  log(`stderr: ${stderr}`);
+});
 
 log(chalk.blue('Hello') + ' World' + chalk.green('!'));
