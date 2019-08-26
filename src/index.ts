64d5029cc355893c339652862cf8c92af591af87
zakallen <hi@zak.dev>
2019-08-25 18:40:11 -0700

    commander and not yargs

--- src/index.ts
+++ src/index.ts
@@ -4,26 +4,19 @@ const { log, time, timeEnd } = console;
 
 import chalk from 'chalk';
 import { exec } from 'child_process';
+import * as commander from 'commander';
 
 import executionStr from './command';
 import processOutput from './process';
 import renderOutput from './render';
 
-import program from 'commander';
-program
-  .version('0.0.1')
-  .description('An application for pizzas ordering')
-
 time('Completed in');
 
-var argv = require('yargs') // eslint-disable-line
-  .usage('Usage: $0 <command> [options]')
-  .command('todo', 'List out only todos')
-  .command('feature', 'List out only features')
-  .command('refactor', 'List out only refactors')
-  .command('bugfix', 'List out only bugfixes')
-  .help('h')
-  .argv;
+commander
+  .version('0.0.1')
+  .description('Todo comments on steroids.')
+
+commander.parse(process.argv);
 
 exec(executionStr(), (error: any, stdout: string): void => {
   if (error) {
